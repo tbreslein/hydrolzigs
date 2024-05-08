7e94c4257ac25553034e928c99978e3c79ffaf00
Tommy Breslein <tommy.breslein@protonmail.com>
2024-05-08 14:19:17 +0200

    add flake.nix

---
+++ flake.nix
@@ -0,0 +1,14 @@
+{
+  description = "1d hydrodynamics solver using riemann methods written in Zig";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShell = pkgs.mkShell { buildInputs = with pkgs; [ zig zls ]; };
+      });
+}
