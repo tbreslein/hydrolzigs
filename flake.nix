1e631d2775ae4994c93d26b3105325d67a522fca
Tommy Breslein <tommy.breslein@protonmail.com>
2024-05-23 13:40:31 +0200

    add nix tools to flake.nix

--- flake.nix
+++ flake.nix
@@ -5,10 +5,14 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = nixpkgs.legacyPackages.${system};
-      in {
-        devShell = pkgs.mkShell { buildInputs = with pkgs; [ zig zls ]; };
-      });
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShell = pkgs.mkShell {buildInputs = with pkgs; [zig zls nil statix alejandra];};
+    });
 }
